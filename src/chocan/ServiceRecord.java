523ba8ddafe316f4c1d0887673d146c684425fc3
Dylan Stokes <167402229+dboogie23@users.noreply.github.com>
2025-12-05 00:55:10 -0600

    UPDATED: Add ServiceRecord class with properties and methods

--- src/chocan/ServiceRecord.java
+++ src/chocan/ServiceRecord.java
@@ -1,5 +1,30 @@
 package chocan;
 
 public class ServiceRecord {
-    
+
+    private String providerNumber;
+    private String memberNumber;
+    private String serviceCode;
+    private String date;
+    private double fee;
+
+    public ServiceRecord(String providerNumber, String memberNumber, String serviceCode, String date, double fee)
+    {
+        this.providerNumber = providerNumber;
+        this.memberNumber = memberNumber;
+        this.serviceCode = serviceCode;
+        this.date = date;
+        this.fee = fee;
+    }
+
+    public String getProviderNumber() { return providerNumber; }
+    public String getMemberNumber() { return memberNumber; }
+    public String getServiceCode() { return serviceCode; }
+    public String getDate() { return date; }
+    public double getFee() { return fee; }
+
+    public String getInfo()
+    {
+        return providerNumber + "|" + memberNumber + "|" + serviceCode + "|" + date + "|" + fee;
+    }
 }
